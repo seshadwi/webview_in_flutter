cad80a49aec4fbca194586a648df20e435ffbbb0
seshadwi <seshaadwi@gamil.com>
2022-09-08 17:38:09 +0700

    add page load events

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+// Delete the package:webview_flutter/webview_flutter.dart import
+import 'src/web_view_stack.dart'; // Add this import
 
 void main() {
   runApp(
@@ -19,22 +19,13 @@ class WebViewApp extends StatefulWidget {
 
 class _WebViewAppState extends State<WebViewApp> {
   @override
-  void initState() {
-    if (Platform.isAndroid) {
-      WebView.platform = SurfaceAndroidWebView();
-    }
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter WebView'),
       ),
-      body: const WebView(
-        initialUrl: 'https://flutter.dev',
-      ),
+      body:
+          const WebViewStack(), // Replace the WebView widget with WebViewStack
     );
   }
 }
